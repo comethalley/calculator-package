4c0c7559349c7cd6b88081a646ae6925fad03d6f
Kenneth D. Bonita <kenneth.bonita07@gmail.com>
2024-07-25 09:26:59 +0800

    Package Configuration

---
+++ src/Providers/CalculatorServiceProvider.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace Comethalley\CalculatorPackage;
+
+use Illuminate\Support\ServiceProvider;
+
+class CalculatorPackage extends ServiceProvider
+{
+    public function register()
+    {
+    }
+
+    public function boot()
+    {
+    }
+}
